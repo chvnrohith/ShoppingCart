452e0bdc8a5fbf3a7b04428a59d7af2077c27c25
chvnrohith <chvenkatanagarohith@gmail.com>
2023-07-30 18:09:39 -0500

    Final changes

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -11,7 +11,7 @@ var (
 
 func Connect() {
 	//prosgret_conname := ("host=db port=5432 user=postgres dbname=postgres password=example sslmode=disable")
-	d, err := gorm.Open("postgres", "akhil:Axlesharma@12@/simplerest?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "root:root@/project?charset=utf8&parseTime=True&loc=Local")
 	//d, err := gorm.Open("mysql", "akhil:Axlesharma@12@/simplerest?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
